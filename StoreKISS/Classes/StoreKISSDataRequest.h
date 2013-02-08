41f0ae34c8537db0eca14dc903fa322bfbaa4442
Misha Karpenko <karpenko.misha@gmail.com>
2013-02-08 17:23:58 +0200

    Added '= 0' for StoreKISSDataRequestStatus enum

--- StoreKISS/Classes/StoreKISSDataRequest.h
+++ StoreKISS/Classes/StoreKISSDataRequest.h
@@ -11,7 +11,7 @@
 @class StoreKISSDataRequest;
 
 typedef enum {
-	StoreKISSDataRequestStatusNew,
+	StoreKISSDataRequestStatusNew = 0,
 	StoreKISSDataRequestStatusStarted,
 	StoreKISSDataRequestStatusFinished
 } StoreKISSDataRequestStatus;
